a1ee09a1ad3148e697ca6c10065ca4321120d826
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-12-01 11:08:54 -0800

    state of font changing between multiple components

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Editor.css';
 
-export default function Editor({ setTitle, setSubtitle }) {
+export default function Editor({ setTitle, setSubtitle, setFont }) {
   return (
     <div className="editor">
       <div className="form-control">
@@ -14,7 +14,7 @@ export default function Editor({ setTitle, setSubtitle }) {
         <label>Subtitle</label>
       </div>
       <div className="form-control">
-        <select>
+        <select onChange={(e) => setFont(e.target.value)}>
           <option value="architect">{"Architect's Daughter"}</option>
           <option value="comforter">Comforter</option>
           <option value="fredoka">Fredoka</option>
